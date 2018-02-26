15bc413327a19f821a7182a6a683f808b82a4b00
joanitagomez@gmail.com <joanitagomez@gmail.com>
2018-02-25 16:41:49 -0800

    Added screenshot

--- src/index.js
+++ src/index.js
@@ -39,7 +39,7 @@ class Page extends React.Component {
   render() {
     return (
       <div className="container">
-        <a href="https://github.com/you">
+        <a href="https://github.com/joanithagomez/Vault-App">
           <img
             className="github-ribbon"
             src="https://camo.githubusercontent.com/c6286ade715e9bea433b4705870de482a654f78a/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f6c6566745f77686974655f6666666666662e706e67"
